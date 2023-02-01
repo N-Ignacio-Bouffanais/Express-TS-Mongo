c7462abe93c2f8a0fbd8b1990a34adc7426461ec
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-02-01 19:31:34 -0300

    [update] swagguer documentation routes and schemas

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,12 +3,15 @@ import express from "express";
 import path from "path";
 import cors from "cors";
 import morgan from "morgan";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 import { task_router } from "./routes/task";
 import db from "./config/mongo";
+import { options } from "./config/swaggerOptions";
 
+const especs = swaggerJSDoc(options)
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 
 app.use(morgan("dev"));
 app.use(
@@ -18,9 +21,12 @@ app.use(
   })
 );
 app.use(express.json());
-
 app.use("/api", task_router);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(especs));
 
 // DB connection
 db().then(() => console.log("Database connected"));
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server on http://localhost:${PORT}`),
+  console.log(`Documentation will be available at http://localhost:${PORT}/docs`)
+});
--- backend/src/routes/task.ts
+++ backend/src/routes/task.ts
@@ -9,10 +9,149 @@ import {
 
 const task_router = Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Task:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: string
+ *          description: the auto-generated id of task
+ *        name:
+ *          type: string
+ *          description: the name of the task
+ *        description:
+ *          type: string
+ *          description: the description of the task
+ *      required:
+ *        - name
+ *        - description
+ *      example:
+ *        id: gQBOyGbxcQy6tEp0aZ78X
+ *        name: My first Task
+ *        description: I have to do Something
+ *    TaskNotFound:
+ *      type: object
+ *      properties:
+ *        msg:
+ *          type: string
+ *          description: A message for the not found task
+ *      example:
+ *        msg: Task was not found
+ *
+ *  parameters:
+ *    taskId:
+ *      in: path
+ *      name: id
+ *      required: true
+ *      schema:
+ *        type: string
+ *      description: the task id
+ */
+/**
+ * @swagger
+ * tags:
+ *  name: Tasks
+ *  description: Tasks endpoint
+ */
+/**
+/**
+ * @swagger
+ * /tasks:
+ *  get:
+ *    summary: Returns a list of tasks
+ *    tags: [Tasks]
+ *    responses:
+ *      200:
+ *        description: the list of tasks
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Task'
+ */
 task_router.get("/tasks", getItems)
+/**
+ * @swagger
+ * /tasks:
+ *  post:
+ *    summary: create a new task
+ *    tags: [Tasks]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Task'
+ *    responses:
+ *      200:
+ *        description: the tasks was successfully created
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Task'
+ *      500:
+ *        description: Some server error
+ *
+ */
 task_router.post("/tasks", postItem);
+/**
+ * @swagger
+ * /tasks/{id}:
+ *  get:
+ *    summary: get a task by Id
+ *    tags: [Tasks]
+ *    parameters:
+ *      - $ref: '#/components/parameters/taskId'
+ *    responses:
+ *      200:
+ *        description: The Found Task
+ *        content:
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/Task'
+ *      404:
+ *        description: the task was not found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/TaskNotFound'
+ */
 task_router.get("/tasks/:id", getItem);
+/**
+ * @swagger
+ * /tasks/:id:
+  *   put:
+  *     summary: Update a task
+  *     tags: [Tasks]
+ */
 task_router.put("/tasks/:id", putItem);
+/**
+ * @swagger
+ * /tasks/{id}:
+ *  delete:
+ *    summary: delete a task by id
+ *    tags: [Tasks]
+ *    parameters:
+ *      - $ref: '#/components/parameters/taskId'
+ *    responses:
+ *      200:
+ *        description: the task was deleted
+ *        content:
+ *          application/json:
+ *            schema:
+ *            $ref: '#/components/schemas/Task'
+ *      404:
+ *        description: the task was not found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/TaskNotFound'
+ *
+ */
 task_router.delete("/tasks/:id", deleteItem);
 
 export { task_router }
